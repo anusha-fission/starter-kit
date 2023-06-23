a70bed48f2f74f442c503a78d2290c38c5150ac9
Anusha Gurajapu <anusha.gurajapu@fissionlabs.com>
2023-06-23 18:22:35 +0530

    record video

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React, {
-  useState,
-} from 'react';
+import React, {useRef, useState} from 'react';
 import {
-  StyleSheet,
+  Alert,
   Dimensions,
   PixelRatio,
+  Text,
+  TouchableHighlight,
+  View,
 } from 'react-native';
 import {
   ViroARSceneNavigator,
@@ -20,202 +21,113 @@ import {
   ViroQuad,
   ViroNode,
   ViroAnimations,
-  ViroConstants
+  ViroConstants,
 } from '@viro-community/react-viro';
+import styles from './AppStyles';
+import RNFS from 'react-native-fs';
+import Share from 'react-native-share';
 
+const App = () => {
+  var path = RNFS.DocumentDirectoryPath + '/viroRecord.txt';
+  const [text, setText] = useState('Initializing AR...');
+  const [isRecording, setIsRecording] = useState(false);
+  const [cameraTransformInfo, setCameraTransformInfo] = useState([]);
+  let ARSceneNav;
 
-const MeasureSceneAR = () => {
-  const [initialized, setInitialized] = useState(false)
-  const [text, setText] = useState('Initializing AR...')
-  const [firstNodePlaced, setFirstNodePlaced] = useState(false)
-  const [distance, setDistance] = useState(null)
+  const onShare = async videoUrl => {
+    try {
+      const result = await Share.open({
+        urls: [`file://${path}`, `file:/${videoUrl}`],
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  };
 
-  const arSceneRef = React.useRef(null)
-  const nodeRef1 = React.useRef(null)
-  const nodeRef2 = React.useRef(null)
-
-  const _onTrackingUpdated = (state, reason) => {
-    // if the state changes to "TRACKING_NORMAL" for the first time, then
-    // that means the AR session has initialized!
-    console.log(state);
-    if (!initialized && state === ViroConstants.TRACKING_NORMAL) {
-      setInitialized(true);
+  function onInitialized(state, reason) {
+    console.log('guncelleme', state, reason, ARSceneNav?.bloomEnabled);
+    if (state === ViroConstants.TRACKING_NORMAL) {
       setText('Hello World!');
-      console.log('tracked');
+    } else if (state === ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
     }
   }
 
-  const handleSceneClick = source => {
-    console.log('handleSceneClick',((Dimensions.get('window').width * PixelRatio.get()) / 2, (Dimensions.get('window').height * PixelRatio.get()) / 2),arSceneRef);
-    arSceneRef.current.performARHitTestWithPoint((Dimensions.get('window').width * PixelRatio.get()) / 2, (Dimensions.get('window').height * PixelRatio.get()) / 2)
-      .then((results) => {
-        console.log('handleSceneClick',results);
-        for (var i = 0; i < results.length; i++) {
-          let result = results[i];
-          if (result.type === "ExistingPlaneUsingExtent") {
-            // We hit a plane, do something!
-            if (firstNodePlaced) {
-              console.log('move two')
-
-              nodeRef2.current.setNativeProps({
-                position: result.transform.position,
-                visible: true,
-              })
-
-              nodeRef1.current.getTransformAsync().then(transform => {
-                console.log(transform.position);
-
-                getDistance(transform.position, result.transform.position)
-              })
-
-            } else {
-              console.log('move one')
-
-              nodeRef2.current.setNativeProps({
-                visible: false
-              });
-
-              nodeRef1.current.setNativeProps({
-                position: result.transform.position,
-                visible: true
-              })
-
-              setFirstNodePlaced(true)
-            }
-          }
-        }
-      }).catch(err=>{
-        console.error('handleSceneClick',err);
-      })
+  function onCameraTransformUpdate(pos) {
+    cameraTransformInfo.push(pos);
+    setCameraTransformInfo(cameraTransformInfo);
   }
 
-  const getDistance = (positionOne, positionTwo) => {
-    // Compute the difference vector between the two hit locations.
-    const dx = positionOne[0] - positionTwo[0];
-    const dy = positionOne[1] - positionTwo[1];
-    const dz = positionOne[2] - positionTwo[2];
-
-    // // Compute the straight-line distance.
-    const distanceMeters = Math.sqrt(dx * dx + dy * dy + dz * dz);
-
-    console.log(distanceMeters * 100)
-
-    setDistance(distanceMeters * 100)
-  }
-
-  const handleDrag = (dragToPos, source) => {
-    nodeRef1.current.getTransformAsync().then(transform => {
-      console.log(transform.position);
-
-      getDistance(transform.position, dragToPos)
-    })
-  }
-
-  return (
-    <ViroARScene
-      ref = {arSceneRef}
-      onTrackingUpdated = {_onTrackingUpdated}
-      onClick = {handleSceneClick}
-    >
-      <ViroNode
-        ref = {nodeRef1}
-        position = {[0, 0, 0]}
-        visible = {false}
-        onClick = {() => {}}
-        onDrag = {() => {}}
-        dragType = "FixedToWorld"
-      >
-      <ViroSpotLight
-        innerAngle = {5}
-        outerAngle = {45}
-        direction = {[0, -1, -0.2]}
-        position = {[0, 3, 0]}
-        color = "#ffffff"
-        castsShadow = {true}
-        influenceBitMask = {2}
-        shadowMapSize = {2048}
-        shadowNearZ = {2}
-        shadowFarZ = {5}
-        shadowOpacity = {0.7}
-      />
-      <Viro3DObject
-       source={require('./assets/emoji_smile.vrx')}
-        position = {[0, 0, 0]}
-        scale = {[0.25, 0.25, 0.25]}
-        type = "VRX"
-        lightReceivingBitMask = {3}
-        shadowCastingBitMask = {2}
-        transformBehaviors = {['billboardY']}
-        resources={[require('./assets/emoji_smile_diffuse.png'),
-                       require('./assets/emoji_smile_specular.png'),
-                       require('./assets/emoji_smile_normal.png')]}
-        />
-      </ViroNode >
-
-      <ViroNode
-        ref = {nodeRef2}
-        position = {[0, 0, 0]}
-        visible = {false}
-        onClick = {() => {}}
-        onDrag = {handleDrag}
-        dragType = "FixedToWorld"
-      >
-        <ViroSpotLight
-          innerAngle = {5}
-          outerAngle = {45}
-          direction = {[0, -1, -0.2]}
-          position = {[0, 3, 0]}
-          color = "#ffffff"
-          castsShadow = {true}
-          influenceBitMask = {2}
-          shadowMapSize = {2048}
-          shadowNearZ = {2}
-          shadowFarZ = {5}
-          shadowOpacity = {0.7}
-        />
-        <Viro3DObject
-         source={require('./assets/emoji_smile.vrx')}
-          position = {[0, 0, 0]}
-          scale = {[0.25, 0.25, 0.25]}
-          type = "VRX"
-          lightReceivingBitMask = {3}
-          shadowCastingBitMask = {2}
-          transformBehaviors = {['billboardY']}
-          resources={[require('./assets/emoji_smile_diffuse.png'),
-                     require('./assets/emoji_smile_specular.png'),
-                     require('./assets/emoji_smile_normal.png')]}
-        />
+  const InitialARScene = () => {
+    return (
+      <ViroARScene
+        onTrackingUpdated={onInitialized}
+        onCameraTransformUpdate={onCameraTransformUpdate}>
         <ViroText
-          text = {distance ? distance.toFixed(2) + 'cm' : ''}
-          scale = {[0.1, 0.1, 0.1]}
-          position = {[0, 0, -0.05]}
-          style = {styles.helloWorldTextStyle}
+          text={text}
+          scale={[0.5, 0.5, 0.5]}
+          position={[0, 0, -1]}
+          style={styles.helloWorldTextStyle}
         />
-      </ViroNode >
-    </ViroARScene>
-  )
-}
+      </ViroARScene>
+    );
+  };
 
+  const recordError = e => {
+    console.error('recordError', e);
+  };
 
-export default () => {
+  const writeCameraTranformFile = () => {
+    console.log(JSON.stringify(cameraTransformInfo));
+    RNFS.writeFile(path, JSON.stringify(cameraTransformInfo), 'utf8')
+      .then(success => {
+        console.log('FILE WRITTEN!');
+        onShare();
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  };
+
+  const handleRecord = async () => {
+    if (ARSceneNav?.sceneNavigator) {
+      if (!isRecording) {
+        setCameraTransformInfo([]);
+        ARSceneNav.sceneNavigator.startVideoRecording(
+          'viroRecord',
+          true,
+          recordError,
+        );
+      } else {
+        const stope = await ARSceneNav.sceneNavigator.stopVideoRecording();
+        writeCameraTranformFile(stope.url);
+        console.log('recording end', stope, path, stope.url);
+      }
+    }
+    setIsRecording(!isRecording);
+  };
+
   return (
-    <ViroARSceneNavigator
-      autofocus = {true}
-      initialScene = {{scene: MeasureSceneAR}}
-      style = {styles.f1}
-    />
+    <View style={{flex: 1}}>
+      <ViroARSceneNavigator
+        ref={nav => (ARSceneNav = nav)}
+        initialScene={{scene: InitialARScene}}
+        autofocus={true}
+      />
+      <TouchableHighlight style={styles.recordButton} onPress={handleRecord}>
+        <Text style={styles.recordText}>{isRecording ? 'Stop' : 'Record'}</Text>
+      </TouchableHighlight>
+    </View>
   );
 };
 
-var styles = StyleSheet.create({
-  f1: {
-    flex: 1
-  },
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-});
+export default App;
---
+++ AppStyles.js
@@ -0,0 +1,30 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  f1: {
+    flex: 1,
+  },
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+  recordButton: {
+    padding: 10,
+    position: 'absolute',
+    bottom: 30,
+    left: '40%',
+    borderRadius: 16,
+    backgroundColor: 'red',
+  },
+  recordText: {
+    textTransform: 'capitalize',
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#fff',
+  },
+});
+
+export default styles;
